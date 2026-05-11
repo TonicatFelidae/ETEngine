c19b5f59aa71ce0842fc35c91a90ec0780741e8c
Tania Felidea <50703992+TonicatFelidae@users.noreply.github.com>
2026-05-11 11:49:10 +0700

    Fix back ground size

--- Plugins/ETSimpleKit/Script/ScreenSizeFixer/BackGroundSizeFix.cs
+++ Plugins/ETSimpleKit/Script/ScreenSizeFixer/BackGroundSizeFix.cs
@@ -1,12 +1,16 @@
 using ET.SupportKit;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(RectTransform))]
-public class BackGroundSizeFix : MonoBehaviour
+[RequireComponent(typeof(Image))]
+public class BackgroundSizeFix : MonoBehaviour
 {
     RectTransform _rectTransform;
+    Image _image;
     public float width = 1;
     public float height = 1;
     public FixType fixType;
@@ -14,14 +18,16 @@ public class BackGroundSizeFix : MonoBehaviour
     private void Awake()
     {
         _rectTransform = GetComponent<RectTransform>();
+        _image = GetComponent<Image>();
         if (enableDebug)
         {
-           Debug.Log($"BackGroundSize {_rectTransform.rect.width}:{_rectTransform.rect.height}") ;
+            Debug.Log($"BackGroundSize {_rectTransform.rect.width}:{_rectTransform.rect.height}");
         }
     }
     private void Start()
     {
         float ratio = width / height;
+        float spriteRatio = _image.sprite.bounds.size.x / _image.sprite.bounds.size.y;
         switch (fixType)
         {
             case FixType.FixRatioBaseOnWidth:
@@ -32,8 +38,13 @@ public class BackGroundSizeFix : MonoBehaviour
                 //_rectTransform.rect = r1;
 
                 break;
-            case FixType.ExactlySize:
+            case FixType.SpriteRatioBaseOnWidth:
+                SetHeight(_rectTransform.rect.width / ratio);
                 break;
+            case FixType.SpriteRatioBaseOnHeight:
+                SetWidth(_rectTransform.rect.height * ratio);
+                break;
+
             default:
                 break;
         }
@@ -46,7 +57,7 @@ public class BackGroundSizeFix : MonoBehaviour
     {
         Vector2 sizeDelta = _rectTransform.sizeDelta;
         sizeDelta.x = size;
-        _rectTransform.sizeDelta= sizeDelta;
+        _rectTransform.sizeDelta = sizeDelta;
     }
     public void SetHeight(float size)
     {
@@ -58,6 +69,7 @@ public class BackGroundSizeFix : MonoBehaviour
     {
         FixRatioBaseOnWidth,
         FixRatioBaseOnHeight,
-        ExactlySize,
+        SpriteRatioBaseOnWidth,
+        SpriteRatioBaseOnHeight,
     }
 }
